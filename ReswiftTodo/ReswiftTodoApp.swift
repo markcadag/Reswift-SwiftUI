77a0d4397d0722c36256167503ce12d37207ae49
mark <markcadag@gmail.com>
2022-09-09 17:20:14 +0800

    Initial Commit

---
+++ ReswiftTodo/ReswiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReswiftTodoApp.swift
+//  ReswiftTodo
+//
+//  Created by iOS Developer on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ReswiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
